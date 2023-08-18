5497a727b525a74877b875335ccec3f0d901f1f8
Marcelo <marcelovqvd@gmail.com>
2023-08-18 09:59:53 -0300

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Button } from './components/Button';
-
 export function App() {
   return (
     <>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
+      <h1>dev</h1>
     </>
   )
 }
