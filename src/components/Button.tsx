618c5256d3e5722ddaffc131884fe3bbf5077132
Marcelo <marcelovqvd@gmail.com>
2023-08-23 17:28:27 -0300

    Páginas e rotas - Componente: Header

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer } from './Button.styles'
-
-interface ButtonProps {
-  action: 'salvar' | 'enviar'
-}
-
-export function Button({ action }: ButtonProps) {
-  return <ButtonContainer>{action}</ButtonContainer>
-}
