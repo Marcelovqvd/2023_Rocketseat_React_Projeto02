302992d1e3dbdae83f6d65aea1cbac9104074a48
Marcelo <marcelovqvd@gmail.com>
2023-12-08 16:41:23 -0300

    estrutura inicial

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,14 +2,12 @@ import { Route, Routes } from "react-router-dom"
 import { DefaultLayout } from "./layouts/DefaultLayout"
 
 import { Home } from "./pages/Home"
-import { Page2 } from "./pages/page2"
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/page2" element={<Page2 />} />
       </Route>
     </Routes>
   )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,3 @@
-import { useState } from "react"
-import { Post } from "../../components/post"
-import { HomeContainer } from "./styles"
-
 export function Home() {
-  const [comments, setComments] = useState([])
-  const [newCommentText, setNewCommentText] = useState("")
-
-  function handleCreateNewComment() {
-    event?.preventDefault()
-    setComments([...comments, newCommentText])
-    setNewCommentText("")
-  }
-
-  function handleNewCommentChange() {
-    setNewCommentText(event?.target.value)
-  }
-
-  return (
-    <HomeContainer>
-      <form onSubmit={handleCreateNewComment}>
-        <input
-          name="comment"
-          value={newCommentText}
-          onChange={handleNewCommentChange}
-        />
-        <button type="submit">Postar comentário</button>
-      </form>
-      {comments.map((i) => {
-        return <Post text={i} />
-      })}
-    </HomeContainer>
-  )
+  return
 }
